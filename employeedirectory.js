15d53416caae7286522be97b9a97882868b5cd61
Walter M <waltermccone88@gmail.com>
2021-03-23 16:40:54 -0500

    Wired up a switch operator to funnel the user into an action depending on the task they select

--- employeedirectory.js
+++ employeedirectory.js
@@ -4,22 +4,64 @@ require('dotenv').config();
 
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-  
-    // Your port; if not 3301
-    port: 3001,
-  
-    // Your username
-    user: process.env.DB_USER,
-  
-    // Your password
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-  });
+  host: 'localhost',
 
-  const dataBaseQuestion = () => {
+  // Your port; if not 3301
+  port: 3001,
 
-    inquirer.
+  // Your username
+  user: process.env.DB_USER,
+
+  // Your password
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+const taskOperation = (data) => {
+  switch (data.databasetask) {
+    case 'View all employees':
+
+      break;
+    case 'View all employees by department':
+
+      break;
+    case 'View all employees by manager':
+
+      break;
+    case 'Add Employee':
+
+      break;
+    case 'Update an employees role':
+
+      break;
+    case 'Update an employee manager':
+
+      break;
+    case 'View departments':
+
+      break;
+    case 'Add a department':
+
+      break;
+    case 'Remove a department':
+
+      break;
+    case 'View Roles':
+
+      break;
+    case 'Add a role':
+
+      break;
+    case 'Remove a role':
+
+      break;
+    default:
+      connection.end()
+      break;
+  }
+}
+const dataBaseQuestion = () => {
+
+  inquirer.
     prompt([
       {
         name: 'databasetask',
@@ -43,14 +85,18 @@ const connection = mysql.createConnection({
         ]
       },
     ])
-  }
 
-  const runDataBase = () => {
+    .then((data) => {
+      taskOperation(data)
+    })
+}
 
-  }
+const runDataBase = () => {
+  dataBaseQuestion()
+}
 
-  connection.connect((err) => {
-    if (err) throw err;
-    // run the start function after the connection is made to prompt the user
-    runDataBase();
-  });
+connection.connect((err) => {
+  if (err) throw err;
+  // run the start function after the connection is made to prompt the user
+  runDataBase();
+});
